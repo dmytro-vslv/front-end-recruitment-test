c4c97c492b56ee41036d0db646dfd36b1c57e1fc
Dmytro Vasyliev <dvcodetime@gmail.com>
2021-08-21 16:48:40 +0200

    Complete task 1

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,1 +1,19 @@
-import '../css/app.css'
+import "../css/app.scss";
+
+// BACON SCRIPT START
+const button = document.querySelector("button");
+const baconSection = document.querySelectorAll("section")[1];
+const baconImage = document.querySelector("img");
+let baconStorage = 11;
+
+button.addEventListener("click", () => {
+  const copyBaconImage = baconImage.cloneNode(true);
+
+  if (baconStorage > 0) {
+    baconSection.appendChild(copyBaconImage);
+    baconStorage--;
+  } else {
+    alert("Out of bacon. Contact admin for more!");
+  }
+});
+// BACON SCRIPT END
